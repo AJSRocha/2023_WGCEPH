d231d75e92a48be67fad0f869069324f02e2a5c8
arocha <alberto.jmas.rocha@gmail.com>
2023-04-17 18:15:59 +0100

    acrescenta 27.10. Corrige um porradao de merdas

--- Intercatch/conversor_IC.R
+++ Intercatch/conversor_IC.R
@@ -8,37 +8,41 @@ source('C://repos/path.R'); path('local')
 ano = 2022
 
 # Importa dados
-# land = read.csv(paste0(dados, 'desembarques//ceph_gux.txt'),
-#                sep = ",", dec = ".")
+land = read.csv(paste0(dados, 'desembarques//desemb_2022.csv'),
+               sep = ",", dec = ".")
 
-load("C:/PNAB/vendas-dia/vd_2022.Rdata")
+# load("C:/dados_pnab/vendas-dia/vd_2022.Rdata")
 
-land = vd_2022 %>%
-  mutate(ANO = year_sale,
-            MES = month_sale,
-            zona = zona,
-            PORTO_PNAB = IPORTO,
-            PORTO_SLV = IPORTO,
-            PORTO_NOME = PORTO,
-            ARTE_EU = EGRUPART,
-            COD_FAO = EESPECIE,
-            DESEMBARQUE = QVENDA) %>% 
-  group_by(ANO, MES, zona, PORTO, ARTE_EU, COD_FAO) %>% 
-  summarise(QESTIMADA = sum(DESEMBARQUE))
+# land = vd_2022 %>%
+#   mutate(ANO = year_sale,
+#             MES = month_sale,
+#             zona = zona,
+#             PORTO_PNAB = IPORTO,
+#             PORTO_SLV = IPORTO,
+#             PORTO_NOME = PORTO,
+#             ARTE_EU = EGRUPART,
+#             COD_FAO = EESPECIE,
+#             DESEMBARQUE = QVENDA) %>% 
+#   group_by(ANO, MES, zona, PORTO, ARTE_EU, COD_FAO) %>% 
+#   summarise(QESTIMADA = sum(DESEMBARQUE))
 
 
 slv = read.csv(paste0(dados, 'especies_slv/slv.csv'))
 portos = read.csv(paste0(dados, 'portos_slv//codigos_portos.csv'))
+portos[182,] = c(40950, "CAIS DO BICO", "csbic", "NW","AVEIRO", "PTCDB", "CDB",NA,NA)
+
 
 # acrescenta codigos fao da tabela SLV 
-# land<-merge(land,slv[,c("ESPECIE_SLV","COD_FAO","FAMILIA")],all.x=T,all.y=F,by="ESPECIE_SLV")
+land = merge(land,slv[,c("ESPECIE_SLV","COD_FAO","FAMILIA")],all.x=T,all.y=F,
+             by.x = 'IESPECIE',
+             by.y ="ESPECIE_SLV")
 
 # acrescenta portos slv
-# land =merge(land,portos[,c("codporto","nome","zona")],
-#       all.x = T,
-#       all.y = F,
-#       by.x = "PORTO_SLV",
-#       by.y = "codporto")
+land =merge(land,portos[,c("codporto","nome","zona")],
+      all.x = T,
+      all.y = F,
+      by.x = "IPORTO",
+      by.y = "codporto")
 
 # restringe desembarques a codigos fao tirados do ASFIS
 ## no dia 26.04 foi acrescentado o SQU, que foi alterado para OMZ
@@ -51,45 +55,51 @@ fao = c("SQC","OCT","OMZ","CTC",
         "OJJ","SQL","CTR","SQU")
 
 # transforma tabela
-# land_export =
-# land %>%
-#   select(nome, zona, COD_FAO, ANO, MES, ARTE_EU, DESEMBARQUE) %>%
-#   filter(ANO == ano) %>%
-#   # remove artes espanholas
-#   filter(ARTE_EU %in% unique(land$ARTE_EU)[!grepl("SP_", unique(land$ARTE_EU))]) %>%
-#   filter(COD_FAO %in% fao) %>%
-#   mutate(zona = factor(case_when(zona == "NW" ~ "27.9.a.c.n",
-#                                  zona == "SW" ~ "27.9.a.c.s",
-#                                  T ~ "27.9.a.s.a")),
-#          # Acerta niveis com formato intercatch
-#          ARTE_EU = factor(case_when(ARTE_EU == 'DTRAWL' ~ "OTB",
-#                                     ARTE_EU == 'PSEINERS' ~ 'PS_SPF_0_0_0',
-#                                     T ~ 'MIS_MIS_0_0_0'))) %>%
-#   group_by(COD_FAO, zona, MES, ARTE_EU) %>%
-#   # desembarques à zona, em kg
-#   summarise(QESTIMADA = sum(DESEMBARQUE, na.rm = T))
+land_export =
+land %>%
+  select(nome, zona, COD_FAO, IANO, IMES, EARTE, QESTIMADA) %>%
+  filter(IANO == ano) %>%
+  # remove artes espanholas
+  filter(EARTE %in% unique(land$EARTE)[!grepl("SP_", unique(land$EARTE))]) %>%
+  filter(COD_FAO %in% fao) %>%
+  mutate(zona = factor(case_when(zona == "NW" ~ "27.9.a.c.n",
+                                 zona == "SW" ~ "27.9.a.c.s",
+                                 zona == 'S' ~ "27.9.a.s.a",
+                                 zona == 'ACORES' ~ '27.10',
+                                 T ~ 'O')),
+         # Acerta niveis com formato intercatch
+         EARTE = factor(case_when(EARTE == 3 ~ "OTB",
+                                    EARTE == 5 ~ 'PS_SPF_0_0_0',
+                                    T ~ 'MIS_MIS_0_0_0'))) %>%
+  group_by(COD_FAO, zona, IMES, EARTE) %>%
+  # desembarques à zona, em kg
+  summarise(QESTIMADA = sum(QESTIMADA, na.rm = T)) %>% 
+  filter(zona != 'O')
 
-land_export = land %>% filter(COD_FAO %in% fao)
+# land_export = land %>% filter(COD_FAO %in% fao)
 
 # altera 'SQU' para 'OMZ' para ficar de acordo com o nome do stock do ICES
-land_export[land_export$COD_FAO == 'SQU',]$COD_FAO = 'OMZ'  
+land_export[land_export$COD_FAO == 'SQU',]$COD_FAO = 'OMZ'
+land_export$zona = as.character(land_export$zona)
 
 # save(land, file="C://Google Drive//Polvices//WGCEPH 2020//desemb_mes_2019.Rdata")
 
 for(j in unique(land_export$COD_FAO)){
-  teste =data.frame()
-  occ =land_export[land_export$COD_FAO==j,]
+  teste_9 = data.frame()
+  teste_10 = data.frame()
+  occ = land_export[land_export$COD_FAO==j,]
   for(i in 1:nrow(occ)){
+    linha =
       paste("HI,","PT,",ano,",","Month,",
-        occ$MES[i],",",
-        occ$ARTE_EU[i],",",
+        occ$IMES[i],",",
+        occ$EARTE[i],",",
         "AreaUnit,",
         occ$zona[i],",",
         "NA,","NA,","-9,","NA",
         "\n",
         "SI,","PT,",ano,",","Month,",
-        occ$MES[i],",",
-        occ$ARTE_EU[i],",",
+        occ$IMES[i],",",
+        occ$EARTE[i],",",
         "AreaUnit,",
         occ$zona[i],",",
         "NA,",
@@ -98,12 +108,20 @@ for(j in unique(land_export$COD_FAO)){
         occ$QESTIMADA[i]/1000,",",
         occ$QESTIMADA[i]/1000,",",
         "-9,",",,",
-        sep="") -> teste[i,1]
+        sep="")
+    if(occ$zona[i] != '27.10'){teste_9[nrow(teste_9)+1,1] = linha}
+    else if(occ$zona[i] == '27.10'){teste_10[nrow(teste_10)+1,1] = linha}
   }
   
-write.table(teste,
+if(nrow(teste_9>0)){  
+write.table(teste_9,
             file=paste0('Intercatch/IC',ano,j,"27_9a_PT_landings.dat",sep="_")
-            ,sep="",row.names = F,col.names = F,quote=F)
+            ,sep="",row.names = F,col.names = F,quote=F)}
+
+if(nrow(teste_10>0)){  
+write.table(teste_10,
+            file=paste0('Intercatch/IC',ano,j,"27_10_PT_landings.dat",sep="_")
+            ,sep="",row.names = F,col.names = F,quote=F)}
 }
 
 # resumo de desembarques OTB
